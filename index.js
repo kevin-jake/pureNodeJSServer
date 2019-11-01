52956a777d111f29e74b7362ae69bce9249897f8
kevin-jake <kevin_jake_04@yahoo.com>
2019-11-01 12:11:50 +0800

    installed express and run npm init

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('x-server-date', new Date())
+    return next();
+})
+
+app.get('/', (req, res, next) => {
+    return res.send('Hello I am a web server');
+
+})
+
+app.get('/time', (req, res, next) => {
+    return res.send(new Date().toString());
+
+})
+
+app.get('/hello', (req, res, next) => {
+    const name = req.query.name;
+    if (!name) {
+        return res.status(400).end();
+    }
+    return res.send(`Hello ${name}`);
+
+});
+
+app.get('/user/:name', (req, res, next) => {
+    return res.send(`Userprofile of ${req.params.name}`);
+
+});
+
+
+app.listen(3000);
+
+
