1eae9ece743a54ccb29321007352c61fea546655
kevin-jake <kevin_jake_04@yahoo.com>
2019-11-01 17:37:38 +0800

    - add layout and images on public folder

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ app.listen(3000);
 
 const routes = require('./routers');
 
+app.use(express.static('public'))
+app.get('/favicon.ico', (req,res,next) => {
+    return res.sendStatus(204)
+})
 app.use('/', routes());
 
 module.export = app;
+
 
